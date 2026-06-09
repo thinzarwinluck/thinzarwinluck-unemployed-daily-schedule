034eb1a38d8ad9bb1494d6a3a696507a19e1cd43
thinzarwinluck <thinzarwinluck.dev@gmail.com>
2026-06-09 15:08:22 +0630

    fix timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import ClockLayoutComponent from "@/components/clock-layout";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between pb-10 pt-5">
-      <h5 className="text-2xl font-bold text-center text-[#F472B6]">
-        🐼 ThinZarWinLuck Unemployed Daily Schedule 🐼
+      <h5 className="text-2xl font-bold text-center text-zinc-700 mb-5">
+        ThinZarWinLuck Unemployed Daily Schedule 🐼 ✨
       </h5>
       <ClockLayoutComponent />
     </div>
